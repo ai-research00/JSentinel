99c0b779cf91e910f0563d3f2ca87df67ee7f700
ai-research00 <ai.research00.00@gmail.com>
2025-06-08 04:57:05 -0400

    Create package.js

---
+++ firefox/package.js
@@ -0,0 +1,12 @@
+{
+  "name": "JSentinel",
+  "fullName": "JSentinel for Firefox",
+  "id": "",
+  "description": "",
+  "author": "Muzan Sano",
+  "license": "Apache License, Version 2.0",
+  "version": "0.1.2a1",
+  "icon": "data/icons/add-on-icon.png",
+  "icon64": "data/icons/icon64.png",
+  "main": "lib/main.js"
+}
